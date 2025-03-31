5d1727da8b1bd060448944e786fcc45183911d2f
Viejyn <diclearslan92@gmail.com>
2025-03-31 15:37:28 +0300

    config.js eklendi

---
+++ config.js
@@ -0,0 +1,6 @@
+window.config = {
+    RAPID_API_KEY: import.meta.env.VITE_RAPID_API_KEY,
+    RAPID_API_URL: import.meta.env.VITE_RAPID_API_URL,
+    RAPID_API_ID: import.meta.env.VITE_RAPID_API_ID,
+};
+
